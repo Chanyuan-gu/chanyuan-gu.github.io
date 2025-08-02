127d412cf2c35e1864defd405fde2dc5862e23ff
Chanyuan Gu <124618667+Chanyuan-gu@users.noreply.github.com>
2025-08-02 14:58:01 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',  // Enable static site export
-  images: {
-    unoptimized: true, // Required for GitHub Pages
-  },
-};
-
-module.exports = nextConfig;
